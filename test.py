c1598d800ebcff3de1ee6cd9fe773dcb301afd56
Forrest Bicker <bickerforrest@gmail.com>
2022-01-30 13:16:20 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 # most_active_cookie.py
 
-from unittest import TestCase
+def test_always_passes():
+    assert True
 
-class TryTesting(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
-
-    # def test_always_fails(self):
-    #     self.assertTrue(False)
+def test_always_fails():
+    assert False
