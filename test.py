992efca295bf3fbcde860e8e14f106662cd96e18
Forrest Bicker <bickerforrest@gmail.com>
2022-01-30 11:56:28 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+# most_active_cookie.py
+
+from unittest import TestCase
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    # def test_always_fails(self):
+    #     self.assertTrue(False)
